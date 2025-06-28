098456b6e8f57e7d9e50b38b54f32334d892a751
sajdakabir <sajda.kbir@gmail.com>
2025-06-28 16:22:30 +0530

    feat: smart contracts

--- smart-contracts/contracts/EResidencyNFT.sol
+++ smart-contracts/contracts/EResidencyNFT.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 /**
  * @title eResidencyNFT
  * @dev Soulbound (non-transferable) ERC721 token for eResidency
  */
-contract EResidencyNFT is ERC721, ERC721URIStorage, Ownable {
+contract EResidencyNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     // Mapping from token ID to metadata
     mapping(uint256 => ResidencyData) private _residencyData;
     
@@ -41,16 +42,16 @@ contract EResidencyNFT is ERC721, ERC721URIStorage, Ownable {
      * @param name The name of the eResident
      * @param citizenshipCountry The country of citizenship
      * @param eResidencyId The eResidency ID
-     * @param tokenURI The URI for the token metadata
+     * @param tokenUri The URI for the token metadata
      */
     function mintNFT(
         address to,
         string memory name,
         string memory citizenshipCountry,
         string memory eResidencyId,
-        string memory tokenURI
+        string memory tokenUri
     ) external onlyOwner returns (uint256) {
-        require(balanceOf(to) == 0, "Address already has an eResidency NFT");
+        require(_tokenOfOwner[to] == 0, "Address already has an eResidency NFT");
         
         _tokenIdCounter++;
         uint256 newTokenId = _tokenIdCounter;
@@ -65,7 +66,7 @@ contract EResidencyNFT is ERC721, ERC721URIStorage, Ownable {
         
         // Mint the token
         _safeMint(to, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
+        _setTokenURI(newTokenId, tokenUri);
         _tokenOfOwner[to] = newTokenId;
         
         emit ResidencyMinted(to, newTokenId, eResidencyId);
@@ -83,7 +84,9 @@ contract EResidencyNFT is ERC721, ERC721URIStorage, Ownable {
         string memory eResidencyId,
         uint256 timestamp
     ) {
-        require(_exists(tokenId), "Token does not exist");
+        // This will revert if token doesn't exist
+        ownerOf(tokenId);
+        
         ResidencyData memory data = _residencyData[tokenId];
         return (data.name, data.citizenshipCountry, data.eResidencyId, data.timestamp);
     }
@@ -93,21 +96,32 @@ contract EResidencyNFT is ERC721, ERC721URIStorage, Ownable {
      * @param owner The address to query
      */
     function tokenOfOwner(address owner) external view returns (uint256) {
-        require(balanceOf(owner) > 0, "Owner has no tokens");
-        return _tokenOfOwner[owner];
+        uint256 tokenId = _tokenOfOwner[owner];
+        require(tokenId != 0, "Owner has no tokens");
+        return tokenId;
     }
     
-    // Override transfer functions to make the token soulbound (non-transferable)
+    // Override _update to prevent transfers and update our ownership mapping
     function _update(
         address to,
         uint256 tokenId,
         address auth
     ) internal override(ERC721) returns (address) {
+        address from = _ownerOf(tokenId);
+        
         // Only allow minting (from address(0)) and burning (to address(0))
-        require(
-            to == address(0) || ownerOf(tokenId) == address(0),
-            "eResidencyNFT: Token is soulbound and cannot be transferred"
-        );
+        if (from != address(0) && to != address(0)) {
+            revert("eResidencyNFT: Token is soulbound and cannot be transferred");
+        }
+        
+        // Update our ownership mapping
+        if (from != address(0)) {
+            delete _tokenOfOwner[from];
+        }
+        if (to != address(0)) {
+            _tokenOfOwner[to] = tokenId;
+        }
+        
         return super._update(to, tokenId, auth);
     }
     
